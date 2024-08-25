d0e7680082d83a45d728eb42ab43d15df67e539f
aragon-cdpr <rafal.tomaszewski@codeperium.io>
2024-08-25 15:10:41 -0700

    todo changes

---
+++ src/main.rs
@@ -0,0 +1,60 @@
+mod todo;
+use todo::Todo;
+
+use std::io::{stdout, Result};
+
+use ratatui::{
+    backend::CrosstermBackend,
+    crossterm::{
+        event::{self, KeyCode, KeyEventKind},
+        terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+        ExecutableCommand,
+    },
+    style::Stylize,
+    widgets::*,
+    Terminal,
+};
+
+fn main() -> Result<()> {
+    let mut todos: Vec<Todo> = vec![
+        Todo::new(
+            String::from("windows sucks"), 
+            false
+        ),
+        Todo::new(
+            String::from("learn rust"), 
+            false
+        )
+    ];
+
+    stdout().execute(EnterAlternateScreen)?;
+    enable_raw_mode()?;
+    let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
+    terminal.clear()?;
+
+    loop {
+        terminal.draw(|frame| {
+            let area = frame.area();
+            frame.render_widget(
+                List::new(todos.iter().map(ListItem::from))
+                    .white()
+                    .on_blue(),
+                area,
+            );
+        })?;
+        if event::poll(std::time::Duration::from_millis(16))? {
+            if let event::Event::Key(key) = event::read()? {
+                if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('t') {
+                    todos[0].toggle();
+                }
+                if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
+                    break;
+                }
+            }
+        }
+    }
+
+    stdout().execute(LeaveAlternateScreen)?;
+    disable_raw_mode()?;
+    Ok(())
+}
---
+++ src/todo/mod.rs
@@ -0,0 +1,57 @@
+use std::fmt::{Debug, Result};
+use ratatui::{
+    text::Text
+};
+    
+pub struct Todo {
+    message: String,
+    done: bool
+}
+
+impl Todo {
+    pub fn get_message(&self) -> &str {
+        &self.message
+    }
+    pub fn is_finished(&self) -> bool {
+        self.done
+    }
+    pub fn toggle(&mut self) -> &mut Self {
+        self.done = !self.done;
+        self
+    }
+    pub fn new(message: String, done: bool) -> Todo {
+        Todo {
+            message,
+            done   
+        }
+    }
+}
+
+impl Debug for Todo {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result {
+        f.debug_struct("Todo")
+            .field("message", &self.message)
+            .field("done", &self.done)
+            .finish()
+    }
+}
+
+impl From<&Todo> for Text<'static> {
+    fn from(todo: &Todo) -> Text<'static> {
+        Text::from(
+            format!("- [{}] {}", 
+                if todo.is_finished() { "x" } else { " " }, 
+                todo.get_message()
+            )
+        )
+    }
+}
+
+impl Clone for Todo {
+    fn clone(&self) -> Self {
+        Todo {
+            message: self.message.clone(),
+            done: self.done
+        }
+    }
+}
