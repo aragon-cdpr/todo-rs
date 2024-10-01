f309f9e19831dfac5f743541c67f43aafc419c11
aragon-cdpr <rafal.tomaszewski@codeperium.io>
2024-10-01 17:53:05 +0200

    delete feature

--- src/main.rs
+++ src/main.rs
@@ -63,20 +63,12 @@ impl UserInput {
     fn delete_char(&mut self) {
         let is_not_cursor_leftmost = self.char_index != 0;
         if is_not_cursor_leftmost {
-            // Method "remove" is not used on the saved text for deleting the selected char.
-            // Reason: Using remove on String works on bytes instead of the chars.
-            // Using remove would require special care because of char boundaries.
-
             let current_index = self.char_index;
             let from_left_to_current_index = current_index - 1;
 
-            // Getting all characters before the selected character.
             let before_char_to_delete = self.input.chars().take(from_left_to_current_index);
-            // Getting all characters after selected character.
             let after_char_to_delete = self.input.chars().skip(current_index);
 
-            // Put all characters together except the selected one.
-            // By leaving the selected one out, it is forgotten and therefore deleted.
             self.input = before_char_to_delete.chain(after_char_to_delete).collect();
             self.move_cursor_left();
         }
@@ -102,6 +94,8 @@ impl UserInput {
 
 
 fn main() -> Result<()> {
+    //TODO: implement db
+    //TODO: get the todos from db
     let mut todos: Vec<Todo> = vec![
         Todo::new(
             String::from("windows doesn't suck(lie)"), 
@@ -113,6 +107,7 @@ fn main() -> Result<()> {
         )
     ];
 
+    //TODO: clean the mess in the code, and seperate to different files
     stdout().execute(EnterAlternateScreen)?;
     enable_raw_mode()?;
     let mut terminal = ratatui::init();
@@ -123,7 +118,7 @@ fn main() -> Result<()> {
 
     loop {
         render_ui(&mut terminal, &todos, &mut state, &app_state, &input)?;
-            
+        //NOTE: Event handling
         if event::poll(std::time::Duration::from_millis(16))? {
             if let event::Event::Key(key) = event::read()? {
                 if key.kind == KeyEventKind::Press {
@@ -136,6 +131,7 @@ fn main() -> Result<()> {
                                 todos[index].toggle();
                             },
                             KeyCode::Char('a') => app_state = AppState::Add,
+                            KeyCode::Char('d') => app_state = AppState::Remove,
                             KeyCode::Esc => continue,
                             KeyCode::Char('q') | _ => break
                         },
@@ -155,7 +151,11 @@ fn main() -> Result<()> {
                             _ => app_state = AppState::Default,
                         },
                         AppState::Remove => {
-                            app_state = AppState::Remove;
+                            //TODO: render widget to ask user if he wants to delete item
+                            //TODO: refresh listing on demand to update list immediately
+                            let index = state.selected().unwrap();
+                            todos.remove(index);
+                            app_state = AppState::Default;
                         }
                     }
                 }
@@ -171,6 +171,7 @@ fn main() -> Result<()> {
 fn render_ui(terminal: &mut DefaultTerminal, list: &Vec<Todo>,  state: &mut ListState, app_state: &AppState, input: &UserInput) -> Result<()> {
     terminal.draw(|frame| {
         let area = frame.area();
+        //NOTE: Default app look
         frame.render_widget(Clear, area);
         frame.render_stateful_widget(
             List::new(list)
@@ -182,8 +183,7 @@ fn render_ui(terminal: &mut DefaultTerminal, list: &Vec<Todo>,  state: &mut List
         );
         match app_state {
             AppState::Add => render_add_widget(frame, area, input),
-            // AppState::Remove=> render_remove_widget(),
-            AppState::Default | _ => {}
+            _ => {}
         }
     })?;
     Ok(())
@@ -214,4 +214,9 @@ fn render_add_widget(frame: &mut Frame<'_>, area: Rect, input: &UserInput) {
         area.y + 1,
     ));
     frame.render_widget(paragraph, area);
+}
+
+fn render_remove_widget() {
+    todo!();
+    //TODO: implement remove widget feature
 }
